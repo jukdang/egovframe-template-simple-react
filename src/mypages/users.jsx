df9a914547f96a1912b8d752316a176443c8d355
jukdang <jukdang479@gmail.com>
2025-10-24 16:58:00 +0900

    참고

--- src/mypages/users.jsx
+++ src/mypages/users.jsx
@@ -1,8 +1,11 @@
 const Users = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900">
+    <div className="min-h-screen flex flex-col items-center justify-center dark:bg-gray-900">
       <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-6">Users Page</h1>
       <p className="text-lg text-gray-600 dark:text-gray-300">Welcome to the Users page!</p>
+
+      {/* https://flowbite.com/docs/components/tables/ */}
+
     </div>
   );
 };
