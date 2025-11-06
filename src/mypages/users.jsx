3d66e0e3ebda919c51c03fb1738b3b87ee3f3958
jukdang <jukdang479@gmail.com>
2025-11-06 15:07:13 +0900

    table 변수 처리

--- src/mypages/users.jsx
+++ src/mypages/users.jsx
@@ -2,6 +2,17 @@ import Header from "./layout/Header";
 import Sidebar from "./layout/Sidebar";
 
 const Users = () => {
+
+  const columns = [
+    "Product Name", "Color", "Category", "Price", "Edit"
+  ];
+
+  const data = [
+    { name: "Apple MacBook Pro 17", color: "Silver", category: "Laptop", price: "$2999" },
+    { name: "Microsoft Surface Pro", color: "White", category: "Laptop PC", price: "$1999" },
+    { name: "Magic Mouse 2", color: "Black", category: "Accessories", price: "$99" },
+  ];
+
   return (
     <>
       <Header />
@@ -18,75 +29,34 @@ const Users = () => {
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                  <th scope="col" className="px-6 py-3">
-                    Product name
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Color
-                  </th>
-                  <th scope="col" className="px-6 py-3" >
-                    Category
-                  </th >
-                  <th scope="col" className="px-6 py-3" >
-                    Price
-                  </th >
-                  <th scope="col" className="px-6 py-3" >
-                    <span className="sr-only" > Edit</span >
-                  </th >
+                  {/* Table Headers */}
+                  {columns.map((column) => (
+                    <th key={column} scope="col" className="px-6 py-3">
+                      {column}
+                    </th>
+                  ))}
                 </tr >
               </thead >
               <tbody>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
-                  </th>
+                {data.map((item) => (
+                  <tr key={item.name} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      {item.name}
+                    </th>
+                    <td className="px-6 py-4" >
+                      {item.color}
+                    </td >
+                    <td className="px-6 py-4" >
+                      {item.category}
+                    </td >
                   <td className="px-6 py-4" >
-                    Silver
-                  </td >
-                  <td className="px-6 py-4" >
-                    Laptop
-                  </td >
-                  <td className="px-6 py-4" >
-                    $2999
+                      {item.price}
                   </td >
                   <td className="px-6 py-4 text-right" >
                     <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline" > Edit</a >
                   </td >
                 </tr >
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600" >
-                  <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white" >
-                    Microsoft Surface Pro
-                  </th >
-                  <td className="px-6 py-4" >
-                    White
-                  </td >
-                  <td className="px-6 py-4" >
-                    Laptop PC
-                  </td >
-                  <td className="px-6 py-4" >
-                    $1999
-                  </td >
-                  <td className="px-6 py-4 text-right" >
-                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline" > Edit</a >
-                  </td >
-                </tr >
-                <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600" >
-                  <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white" >
-                    Magic Mouse 2
-                  </th >
-                  <td className="px-6 py-4" >
-                    Black
-                  </td >
-                  <td className="px-6 py-4" >
-                    Accessories
-                  </td >
-                  <td className="px-6 py-4" >
-                    $99
-                  </td >
-                  <td className="px-6 py-4 text-right" >
-                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline" > Edit</a >
-                  </td >
-                </tr >
+                ))}
               </tbody >
             </table >
           </div >
